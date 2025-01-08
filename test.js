17d520717e8f422f5358f13faefb158cd0cf1b34
Rémy Fevry <remyfvy@gmail.com>
2025-01-08 15:28:33 +0000

    First

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ exports.handler = async (event, context) => {
     if (event.httpMethod === 'GET') {
       try {
         // Process the GET request as needed
-        const data = fetch('https://rt.data.gov.hk/v2/transport/citybus/eta/CTB/002745/26')
+        data =  await (fetch('https://rt.data.gov.hk/v2/transport/citybus/eta/CTB/002745/26')
             .then((response) => response.json())
-            .then((data) =>  data["data"].map((d, i) => ({
-    i: Math.round((new Date(d["eta"]) - new Date(d["data_timestamp"])) / (60 * 1000))
+            .then((data) =>  data["data"].map((d, idx) => ({
+    [idx]: Math.round((new Date(d["eta"]) - new Date(d["data_timestamp"])) / (60 * 1000))
 }))
-            );
+            ));
   
         // Return the data as the response
         return {
