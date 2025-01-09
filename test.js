72f675b4026c696027ffeb387ef1a278aece7c92
Rémy Fevry <remyfvy@gmail.com>
2025-01-09 00:53:18 +0000

    First

--- test.js
+++ test.js
@@ -5,14 +5,14 @@ exports.handler = async (event, context) => {
         data =  await (fetch('https://rt.data.gov.hk/v2/transport/citybus/eta/CTB/002745/26')
             .then((response) => response.json())
             .then((data) =>  data["data"].map((d, idx) => ({
-    [idx]: Math.round((new Date(d["eta"]) - new Date(d["data_timestamp"])) / (60 * 1000))
+    ["eta"+idx]: Math.round((new Date(d["eta"]) - new Date(d["data_timestamp"])) / (60 * 1000))
 }))
             ));
   
         // Return the data as the response
         return {
           statusCode: 200,
-          body: JSON.stringify(data),
+          body: JSON.stringify({result:data}),
         };
       } catch (error) {
         // Return an error response if there was an issue processing the request
